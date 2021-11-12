d4d7a0f4dfd3423bc8d1e39de9660f96d42d200c
vandervalsilva <vanderval.silva@ab-inbev.com>
2021-11-11 21:08:27 -0300

    First commit

--- complete/src/main/java/com/example/springboot/SlackController.java
+++ complete/src/main/java/com/example/springboot/SlackController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SlackController {
 
-	@GetMapping("/")
+	@GetMapping("/index")
 	public String index() {
 		return "SlackTest from Spring Boot!";
 	}
